6af7dca940cb250ddd78d7cc410249913853b919
mostly_harmless <yayuneko@163.com>
2023-03-22 09:38:57 +0800

    update

--- src/.vitepress/theme/components/Pagination.vue
+++ src/.vitepress/theme/components/Pagination.vue
@@ -59,6 +59,7 @@ watch(page, () => {
 </script>
 
 <template>
+  {{ page }} {{ typeof page }}
   <div class="pagination">
     <div class="prev-btn" v-if="!isFirstPage" @click="prev">
       <i class="iconfont icon-xiangzuo1" />
@@ -81,6 +82,7 @@ watch(page, () => {
           v-if="Math.abs(page - num) < 3"
         >
           {{ num }}
+          {{ typeof num }}
         </li>
       </template>
       <li v-if="rangeEnd > 3"><i class="iconfont icon-gengduo"></i></li>
